8f9cd1fd75cdf43103ff06fdf2b10b67f760d9ce
Nico Hülscher <25116822+eweren@users.noreply.github.com>
2022-12-02 09:14:23 +0100

    v0.0.6

--- index.js
+++ index.js
@@ -8,22 +8,17 @@
   extends: ['./default.yml'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': 'error',
     'import/no-anonymous-default-export': 'off',
     'react-hooks/exhaustive-deps': 'off',
     'react/react-in-jsx-scope': 'off',
     'sort-keys-fix/sort-keys-fix': 'error',
     '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/no-shadow': [
-      'error'
-    ],
     'jsdoc/check-tag-names': 'off',
     '@typescript-eslint/no-shadow': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'prettier/prettier': 2
   },
   settings: {
-    'svelte3/typescript': () => require('typescript'),
     'svelte3/typescript': true,
   }
 };
